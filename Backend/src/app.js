41c94804d801cb8f43fa15966eada73cd8a181bf
Rrohandewangan <dewanganrohan126@gmail.com>
2026-04-28 02:19:06 +0530

    Backend

---
+++ Backend/src/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express()
+
+// app.get("/", (req, res) => {
+//     res.send("Backend is working")
+// });
+
+// app.listen(process.env.PORT, () => {
+//     console.log(`server is running on the port ${process.env.PORT}`)
+// })
+
+export default app
+

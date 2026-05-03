64010a06a312c54ff95f61ac6f8ce7afb1354b75
Rohan Dewangan <dewanganrohan126@gmail.com>
2026-05-03 06:35:37 +0530

    Delete Backend directory

--- Backend/src/repositories/user.repo.js
+++
@@ -1,7 +0,0 @@
-import { UserModel } from "../models/user.model.js";
-
-export const userRepo = {
-  create: (doc) => UserModel.create(doc),
-  findByEmail: (email) => UserModel.findOne({ email: email.toLowerCase() }),
-  findById: (id) => UserModel.findById(id),
-};
--- Backend/src/services/auth/password.service.js
+++
@@ -1,6 +0,0 @@
-import bcrypt from "bcryptjs";
-
-export const passwordService = {
-  hash: (plain) => bcrypt.hash(plain, 12),
-  verify: (plain, hash) => bcrypt.compare(plain, hash),
-};
